3aaaccaee5f18cdff5e646bd62dd3f613882a161
Vadim <v.kuzmin@xlab-game.com>
2022-12-16 02:14:20 +0300

    saveHP

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    /*[SerializeField]
+    private Image healthBarFilling;
+    [SerializeField]
+    Dragon health;*/
+}
